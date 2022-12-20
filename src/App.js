49c8e86a66451968e77759e65a6bd13b9fce4a9d
Laura Spina <laura@bravoure.nl>
2022-12-20 19:18:19 +0100

    feat: build

--- src/App.js
+++ src/App.js
@@ -139,7 +139,7 @@ function App() {
 
               <a href={CV} target="_blank" rel="noreferrer">
                 <button class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-2xl group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
-                  <span class="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-2xl group-hover:bg-opacity-0">
+                  <span class="w-full md:w-auto relative px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-2xl group-hover:bg-opacity-0">
                     Download CV
                   </span>
                 </button>
